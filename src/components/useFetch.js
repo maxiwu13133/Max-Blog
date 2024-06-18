09c45d6119130d6546c2d3bf5659d280449c299a
max <maximilianwu0828@gmail.com>
2024-06-18 16:37:32 -0700

    fetch data and update

---
+++ src/components/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+
+const useFetch = (url) => {
+
+  const [data,  setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (!res.ok) {
+          throw Error('could not fetch data for that resource');
+        }
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      })
+  }, [url]);
+
+  return {
+    data,
+    isPending,
+    error
+  }
+}
+
+export default useFetch
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 // Components
 import BlogList from '../components/BlogList'
+import useFetch from '../components/useFetch';
 
 const Home = () => {
-  const [blogs,  setBlogs] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      })
-  }, []);
+  const { data: blogs , isPending, error } = useFetch('http://localhost:8000/blogs')
 
   return ( 
     <div className="home">
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
     </div>
    );
 }
