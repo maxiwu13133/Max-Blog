78f0073a1d3cb8c3eec3fb46f544621a1c45eb1a
max <maximilianwu0828@gmail.com>
2024-06-18 14:48:15 -0700

    page layout skeleton

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+// Components
+import BlogList from '../components/BlogList'
+
+const Home = () => {
+  const [blogs,  setBlogs] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      })
+  }, []);
+
+  return ( 
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+   );
+}
+ 
+export default Home;
