38c328b7b1d30641789685875b9ead277b64ad31
max <maximilianwu0828@gmail.com>
2024-06-18 17:33:42 -0700

    new blog form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,46 @@
+import { useState } from 'react';
+
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('maxi');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  }
+
   return ( 
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={ title }
+          onChange={(e) => setTitle(e.target.value)} 
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={ body }
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <select
+          value={ author }
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="maxi">maxi</option>
+          <option value="max">max</option>
+          <option value="maximilian">maximilian</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
